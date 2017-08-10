eaf84081d74dba473669d3edeb7fa22b34563517
vibhu98 <sukritji@gmail.com>
2017-08-09 18:34:18 -0700

    Create about-us.php

---
+++ ui/about-us.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>About US | Sukrit Shukla</title>
+<script>
+var d=new Date();
+document.getElementById("date").innerHTML=d;
+</script>
+</head>
+<body>
+<div>
+<a href="/">Home</a>
+</div>
+<hr>
+<div><h1>About Us</h1></div>
+<div>
+<p id="date"> </p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+</html>
